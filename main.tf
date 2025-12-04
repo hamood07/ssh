a6897876edcb564028d2f28504a9a1793b8f66af
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-12-04 23:22:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm_instance" {
   name         = "jenkins-vm"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  zone         = "us-central1-c"
 
   boot_disk {
     initialize_params {
