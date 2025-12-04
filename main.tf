6161063130230f212c369f1185efac4342a765d5
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-12-04 19:42:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,4 +8,8 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
-}
+ boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
