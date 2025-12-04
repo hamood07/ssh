65325920e1b64aba5f42ecd5d37723a2823dd986
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-12-04 22:51:50 +0530

    Change VM zone from us-east1-b to us-west3

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm_instance" {
   name         = "jenkins-vm"
   machine_type = "e2-medium"
-  zone         = "us-east1-b"
+  zone         = "us-west3"
 
   boot_disk {
     initialize_params {
