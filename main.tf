546bff4be696f603e08537aa0ad6ea357ecc7708
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-12-04 23:53:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "vm_instance" {
   name         = "jenkins-vm"
   machine_type = "e2-medium"
-  zone         = var.region  # use region as zone for simplicity (adjust if needed)
+  zone         = var.zone  # use zone variable
 
   boot_disk {
     initialize_params {
